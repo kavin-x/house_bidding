50427f21f9f1db05732641998cf65abcdb368514
kavin <109793186+kavin-x@users.noreply.github.com>
2023-03-12 20:17:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import { Container } from '@chakra-ui/react'
 
-import Header from './components/Header/Header';
+import Header from './components/header/Header';
 import Home from './routes/Home';
 import PropertyDetails from './routes/PropertyDetails';
 import Footer from './components/Footer'
